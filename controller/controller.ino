422d327ca7979549100dc9a440dd2bb51c4a7312
fsommer95 <florian.sommer@st.ovgu.de>
2019-05-15 19:33:19 +0200

    add themperatur

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,23 +1,58 @@
 #include <ESP8266WiFi.h>
 #include "WifiSettings.h"    
-   
-
-int wifiStatus;
-
+#include "DHT.h"
 
 
 #define LED D0 //LED PORT
 
+//DHT dht = DHT(D1, DHT11, 6); // DHT(uint8_t pin, uint8_t type, uint8_t count=6);
+DHT dht;
+int wifiStatus;
+
 void setup() {
- pinMode(LED, OUTPUT); // Port aus Ausgang schalten
+  
+   pinMode(LED, OUTPUT); // Port aus Ausgang schalten
+
+   dht.setup(D1);   /* D1 is used for data communication */
  
-       Serial.begin(115200);\
-      delay(200);
+   Serial.begin(115200);
+   delay(200);   
+
+   connectWifi();
+
+   Serial.println("Status\tHumidity (%)\tTemperature (C)\t(F)");
+
+    
+}
+
+void loop() {
+      checkWifi();
+      checkValues();
+      delay(10000);
+}
+
+
+void checkWifi() {
+  
+   wifiStatus = WiFi.status();
+
+   if(wifiStatus == WL_CONNECTED){
+      Serial.println("");
+      Serial.println("Your ESP is connected!");  
+      Serial.println("Your IP address is: ");
+      Serial.println(WiFi.localIP());        
+      }
       
-     
-     
-      // We start by connecting to a WiFi network
-     
+    else{
+      Serial.println("");
+      Serial.println("WiFi not connected");
+      connectWifi();
+    }  
+ }
+
+ void connectWifi()  {
+  
+      // We start by connecting to a WiFi network     
       Serial.println();
       Serial.println();
       Serial.print("Your are connecting to;");
@@ -26,37 +61,24 @@ void setup() {
       WiFi.begin(ssid, password);
       
       while (WiFi.status() != WL_CONNECTED) {
-        delay(250);
+        delay(500);
         digitalWrite(LED, HIGH); //Led port einschlaten
-        delay(250);
-        digitalWrite(LED, LOW); //Led port einschlaten
+        delay(500);
+        digitalWrite(LED, LOW); //Led port ausschlaten
         
         Serial.print(".");
       }
+   }
 
-
+  void checkValues(){
+     delay(dht.getMinimumSamplingPeriod()); /* Delay of amount equal to sampling period */
+      float humidity = dht.getHumidity();/* Get humidity value */
+      float temperature = dht.getTemperature();/* Get temperature value */
+      Serial.print(dht.getStatusString());/* Print status of communication */
+      Serial.print("\t");
+      Serial.print(humidity, 1);
+      Serial.print("\t\t");
+      Serial.print(temperature, 1);
+      Serial.print("\t\t");
  
- }
-
-void loop() {
- digitalWrite(LED, LOW); //Led port ausschalten
- delay(10000); //1 Sek Pause
- 
- delay(1000);
-
- wifiStatus = WiFi.status();
-
- if(wifiStatus == WL_CONNECTED){
-    Serial.println("");
-    Serial.println("Your ESP is connected!");  
-    Serial.println("Your IP address is: ");
-    Serial.println(WiFi.localIP());  
-    }
-  else{
-    Serial.println("");
-    Serial.println("WiFi not connected");
-    }
-  delay(1000); // check for connection every once a second
-
- 
- }
+  }
